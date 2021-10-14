6fcc513d56261f7f8a6b347d39d39651ebd6331e
Heekyung Do <dhk0729@gmail.com>
2021-10-15 00:33:37 +0900

    List rendering

--- main.js
+++ main.js
@@ -3,7 +3,24 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inStock: false,
+
+      //   solution
+      url: "https://github.com/heekyungdo",
+      //   solution
+
+      //   inStock: false,
+      inventory: 0,
+
+      //   solution
+      onSale: true,
+      //   solution
+
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
